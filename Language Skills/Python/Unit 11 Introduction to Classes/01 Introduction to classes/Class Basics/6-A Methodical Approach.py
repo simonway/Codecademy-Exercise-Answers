74088769a0dee33c34036c3f81fe809e7cdfba82
simonway <simon_way@sohu.com>
2017-03-23 13:41:07 +0800

    Add files via upload

---
+++ Language Skills/Python/Unit 11 Introduction to Classes/01 Introduction to classes/Class Basics/6-A Methodical Approach.py
@@ -0,0 +1,15 @@
+#!C:\python34\python
+
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    # Add your method here!
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal("Jack", 4)
+hippo.description
